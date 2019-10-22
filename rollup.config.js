9858dbdfa44974b3fbc7acc5cb672d3c98f8b326
Arthur Puyou <arthur@puyou.fr>
2019-10-22 23:37:17 +0200

    Inherit from ThumbnailGenerator

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,8 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import pkg from './package.json';
 
 export default [
   {
     input: 'src/index.js',
-    output: {
-      name: 'uppyBlurUp',
-      file: pkg.browser,
-      format: 'umd',
-    },
-    plugins: [resolve(), commonjs()],
-  },
-  {
-    input: 'src/index.js',
-    external: ['ms'],
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' },
